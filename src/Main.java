6ea7bc709090351a1d894587b837e4f536da2e02
Shan Oovattil <shan.oovattil@gmail.com>
2023-10-15 19:11:27 +0530

    OOPS Data Abstraction Demo
    OOPS Data Abstraction Demo

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+// Sample application for Abstract Class - Sample1
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Car obj = new Honda();
+
+        obj.stop();
+    }
+}
+
